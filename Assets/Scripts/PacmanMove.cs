c7dfc0949a2cedd4dbcc5b3d2e8efad00f0de5d2
ooooooomygosh <2846387782@qq.com>
2024-09-24 23:34:12 +1000

    script 1

---
+++ Assets/Scripts/PacmanMove.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed = 0.35f;
+    private Vector2 dest = Vector2.zero;
+
+
+    private void Start()
+    {
+        dest = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 temp = Vector2.MoveTowards(transform.position,dest,speed);
+        GetComponent<Rigidbody2D>().MovePosition(temp);
+        if (Input.GetKey(KeyCode.UpArrow)||Input.GetKey(KeyCode.W))
+        {
+            dest = (Vector2)transform.position + Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.DownArrow)||Input.GetKey(KeyCode.S))
+        {
+            dest = (Vector2)transform.position + Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)||Input.GetKey(KeyCode.A))
+        {
+            dest = (Vector2)transform.position + Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow)||Input.GetKey(KeyCode.D))
+        {
+            dest = (Vector2)transform.position + Vector2.right;
+        }
+        
+    }
+
+}
