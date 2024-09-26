dafeb3b1354b031d6d58a3488de1d8eb150f827d
ooooooomygosh <2846387782@qq.com>
2024-09-26 12:16:14 +1000

    Perfecting Pac-Man's Movement

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -16,25 +16,32 @@ public class NewBehaviourScript : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector2 temp = Vector2.MoveTowards(transform.position,dest,speed);
+        Vector2 temp = Vector2.MoveTowards(transform.position, dest, speed);
         GetComponent<Rigidbody2D>().MovePosition(temp);
-        if (Input.GetKey(KeyCode.UpArrow)||Input.GetKey(KeyCode.W))
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
             dest = (Vector2)transform.position + Vector2.up;
         }
-        if (Input.GetKey(KeyCode.DownArrow)||Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
             dest = (Vector2)transform.position + Vector2.down;
         }
-        if (Input.GetKey(KeyCode.LeftArrow)||Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             dest = (Vector2)transform.position + Vector2.left;
         }
-        if (Input.GetKey(KeyCode.RightArrow)||Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             dest = (Vector2)transform.position + Vector2.right;
         }
-        
+        Vector2 dir = dest - (Vector2)transform.position;
+        GetComponent<Animator>().SetFloat("DIR X",dir.x);
+        GetComponent<Animator>().SetFloat("DIR Y",dir.y);
+
+
+
     }
+
 
 }
