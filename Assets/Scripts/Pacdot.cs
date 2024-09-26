b3a5654f15bddfe1905d6487093ffe2f73c635cb
ooooooomygosh <2846387782@qq.com>
2024-09-26 13:03:46 +1000

    Implemented Pac-Man my way

---
+++ Assets/Scripts/Pacdot.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Pacdot : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Pacman")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
