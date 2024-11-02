42941e51bf1645c644805858b6cfb630a21ea4b5
ooooooomygosh <2846387782@qq.com>
2024-11-02 23:12:13 +1100

    1

--- Assets/Scripts/Pacdot.cs
+++ Assets/Scripts/Pacdot.cs
@@ -2,16 +2,12 @@ using UnityEngine;
 
 public class Pacdot : MonoBehaviour
 {
-
     public AudioSource pelletEatenSound; // 添加音频源变量
-
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == "Pacman")
         {
-
-
             if (pelletEatenSound != null) // 检查是否分配了音频源
             {
                 pelletEatenSound.Play(); // 播放声音
@@ -20,4 +16,3 @@ public class Pacdot : MonoBehaviour
         }
     }
 }
-
